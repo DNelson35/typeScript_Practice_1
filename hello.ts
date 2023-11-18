0afc678c34ed8ec76b36dcba56d33f00c9385c92
Damien Nelson <dhnisaman@gmail.com>
2023-11-18 17:10:07 -0500

    "learning about typescript"

---
+++ hello.ts
@@ -0,0 +1,12 @@
+
+
+
+function greet(person: string, date: Date){
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+}
+
+greet("brandon", new Date())
+
+let msg = "Hey everyone!"
+
+console.log(msg)
