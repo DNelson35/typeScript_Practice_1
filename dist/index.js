4c643673340f05fe31d210b5432a53b33a7303c2
Damien Nelson <dhnisaman@gmail.com>
2023-11-24 23:04:31 -0500

    simple calc functionality

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,33 @@
 "use strict";
 if (typeof window !== 'undefined') {
     const body = document.querySelector(".body");
-    console.log(body);
     const numberButtons = document.createElement("div");
     numberButtons.className = "number-buttons";
     const calculator = document.createElement("div");
     calculator.className = "calculator";
-    const form = document.createElement('from');
+    const form = document.createElement('form');
     const input = document.createElement('input');
     form.className = "form";
     input.className = "input";
-    numberButtons === null || numberButtons === void 0 ? void 0 : numberButtons.appendChild(form);
+    numberButtons.appendChild(form);
     form.style.gridColumn = 'span 4';
     form.appendChild(input);
     if (body !== null) {
         body.append(calculator);
     }
-    const numbers = ["AC", "+/-", "%", "/", "7", "8", "9", "X", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
-    const buttons = CreateDivArr(numbers);
+    const numbers = ["AC", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "0", ".", "="];
+    const buttons = createDivArr(numbers);
     buttons.forEach(button => numberButtons.append(button));
     body === null || body === void 0 ? void 0 : body.append(numberButtons);
-    function CreateDivArr(arr) {
-        const divArr = [];
-        arr.forEach((n) => {
-            let div = document.createElement("div");
-            if (div !== null) {
-                div.className = "num-button";
-                div.textContent = n.toString();
-                div.addEventListener("click", (e) => {
-                    console.log(e.target);
-                });
-            }
+    let currentInput = "";
+    let operation = null;
+    let firstNumber = null;
+    function createDivArr(arr) {
+        return arr.map(n => {
+            const div = document.createElement("div");
+            div.className = "num-button";
+            div.textContent = n.toString();
+            div.addEventListener("click", handleButtonClick);
             if (isNaN(Number(n)) && !["AC", "+/-", "%", "."].includes(n.toString())) {
                 div.style.backgroundColor = 'orange';
             }
@@ -45,8 +42,39 @@ if (typeof window !== 'undefined') {
             else {
                 div.style.backgroundColor = "gray";
             }
-            divArr.push(div);
+            return div;
         });
-        return divArr;
+    }
+    let expression = "";
+    function handleButtonClick(e) {
+        const target = e.target;
+        const value = target.textContent;
+        if (value !== null) {
+            if (!isNaN(Number(value))) {
+                expression += value;
+            }
+            else if (["+", "-", "*", "/"].includes(value)) {
+                expression += value;
+            }
+            else if (value === ".") {
+                if (!expression.includes(".")) {
+                    expression += value;
+                }
+            }
+            else if (value === "=") {
+                let result = eval(expression);
+                expression = result.toString();
+            }
+            else if (value === "AC") {
+                expression = "";
+            }
+            else if (value === "+/-") {
+                expression = (-Number(expression)).toString();
+            }
+            else if (value === "%") {
+                expression = (Number(expression) / 100).toString();
+            }
+            input.value = expression;
+        }
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,85 @@
 if (typeof window !== 'undefined') {
-    const body = document.querySelector(".body")
-    console.log(body)
-    const numberButtons = document.createElement("div")
-    numberButtons.className = "number-buttons"
-    const calculator = document.createElement("div")
-    calculator.className = "calculator"
-    const form = document.createElement('from')
-    const input = document.createElement('input')
-    form.className = "form"
-    input.className = "input"
-    numberButtons?.appendChild(form)
-    form.style.gridColumn = 'span 4'
-    form.appendChild(input)
-    
+   const body: HTMLElement | null = document.querySelector(".body");
+   const numberButtons: HTMLDivElement = document.createElement("div");
+   numberButtons.className = "number-buttons";
 
-    if (body !== null){
-        body.append(calculator)
-    }
+   const calculator: HTMLDivElement = document.createElement("div");
+   calculator.className = "calculator";
 
-    const numbers = ["AC", "+/-", "%", "/", "7", "8", "9","X", "4", "5", "6","-", "1", "2", "3","+", "0", ".",   "="]
+   const form: HTMLFormElement = document.createElement('form');
+   const input: HTMLInputElement = document.createElement('input');
+   form.className = "form";
+   input.className = "input";
 
-    const buttons = CreateDivArr(numbers)
+   numberButtons.appendChild(form);
+   form.style.gridColumn = 'span 4';
+   form.appendChild(input);
 
-    buttons.forEach(button => numberButtons.append(button))
+   if (body !== null){
+       body.append(calculator);
+   }
 
-    body?.append(numberButtons)
+   const numbers: string[] = ["AC", "+/-", "%", "/", "7", "8", "9","*", "4", "5", "6","-", "1", "2", "3","+", "0", ".", "="];
 
-    function CreateDivArr(arr: string[]): HTMLElement[]{
-        const divArr: HTMLElement[] = []
-        arr.forEach((n) => {
-            let div = document.createElement("div")
-            if(div !== null){
-                div.className = "num-button"
-                div.textContent = n.toString()
-                div.addEventListener("click", (e) => {
-                    console.log(e.target)
-                })
-            }
-            if(isNaN(Number(n)) && !["AC", "+/-", "%", "."].includes(n.toString())){
-                div.style.backgroundColor = 'orange'
-            }else if(["AC", "+/-", "%"].includes(n.toString())){
-                div.style.backgroundColor = "#4d4238"
-            }else if (n === "0"){
-                div.style.backgroundColor = "gray"
-                div.style.gridColumn = "span 2"
-                div.style.width = "125px"
-            }else{
-                div.style.backgroundColor = "gray"
-            }
-            divArr.push(div)
-        })
-        return divArr  
-    }
+   const buttons: HTMLDivElement[] = createDivArr(numbers);
+
+   buttons.forEach(button => numberButtons.append(button));
+
+   body?.append(numberButtons);
+
+   let currentInput: string = "";
+   let operation: string | null = null;
+   let firstNumber: string | null = null;
+
+   function createDivArr(arr: string[]): HTMLDivElement[]{
+       return arr.map(n => {
+           const div: HTMLDivElement = document.createElement("div");
+           div.className = "num-button";
+           div.textContent = n.toString();
+           div.addEventListener("click", handleButtonClick);
+
+           if(isNaN(Number(n)) && !["AC", "+/-", "%", "."].includes(n.toString())){
+               div.style.backgroundColor = 'orange';
+           }else if(["AC", "+/-", "%"].includes(n.toString())){
+               div.style.backgroundColor = "#4d4238";
+           }else if (n === "0"){
+               div.style.backgroundColor = "gray";
+               div.style.gridColumn = "span 2";
+               div.style.width = "125px";
+           }else{
+               div.style.backgroundColor = "gray";
+           }
+           return div;
+       });
+   }
+
+   let expression = "";
+
+   function handleButtonClick(e: Event) {
+      const target = e.target as HTMLElement;
+      const value = target.textContent;
+      if (value !== null) {
+          if (!isNaN(Number(value))) {
+              expression += value;
+          } else if (["+", "-", "*", "/"].includes(value)) {
+              expression += value;
+          } else if (value === ".") {
+              if (!expression.includes(".")) {
+                  expression += value;
+              }
+          } else if (value === "=") {
+              let result: number = eval(expression);
+              expression = result.toString();
+          } else if (value === "AC") {
+              expression = "";
+          } else if (value === "+/-") {
+              expression = (-Number(expression)).toString();
+          } else if (value === "%") {
+              expression = (Number(expression) / 100).toString();
+          }
+          input.value = expression;
+      }
+   }
+   
+ 
 }
